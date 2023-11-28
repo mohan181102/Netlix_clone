7c363d0fa561e53331606f14b5e5bd80fefad29f
manmohan <manmohan33chandrakar@gmail.com>
2023-11-28 10:21:42 +0530

    some change 28/11/2002

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,70 @@ import React from "react";
 import Row from "./Row/Row";
 import requist from "./request";
 import Banner from "./Banner/Banner";
-import "./App.css"
+import "./App.css";
+import LoadingBar from "react-top-loading-bar";
 import Nav from "./Header/Header";
+import { useState } from "react";
 
+function App() {
+  const [progress, setprogress] = useState(0);
 
-function App(){
   return (
     <div className="app">
       {/* nav */}
-      <Nav/>
+
+      <Nav />
+      <LoadingBar
+        color="red"
+        progress={progress}
+        onLoaderFinished={(e) => setprogress(0)}
+      />
       {/*Banner  */}
-      <Banner/>
-      <Row title="Netflix orignals" fetchurl={requist.fetch_Netflix_Orignals}/>
-      <Row title="Trending now" backdrop={true} fetchurl={requist.fetchtrending}/>
-      <Row title="Top rated" backdrop={true} fetchurl={requist.fetch_Top_Rated}/>
-      <Row title="Action" backdrop={true} fetchurl={requist.fetch_Action_movies}/>
-      <Row title="Comedy" backdrop={true} fetchurl={requist.fetch_Comedy_movies}/>
-      <Row title="Horror" backdrop={true} fetchurl={requist.fetcht_Horror_movies}/>
-      <Row title="Romantic" backdrop={true} fetchurl={requist.fetcht_Romantic_movies}/>
-      <Row title="Documentries" backdrop={true} fetchurl={requist.fetcht_Documentaries}/>
+      <Banner />
+
+      <Row title="Netflix orignals" fetchurl={requist.fetch_Netflix_Orignals} />
+      <Row
+        title="Trending now"
+        backdrop={true}
+        fetchurl={requist.fetchtrending}
+      />
+      <Row
+        title="Top rated"
+        backdrop={true}
+        fetchurl={requist.fetch_Top_Rated}
+      />
+
+      <Row
+        title="Action"
+        backdrop={true}
+        fetchurl={requist.fetch_Action_movies}
+      />
+      <Row
+        title="Comedy"
+        backdrop={true}
+        fetchurl={requist.fetch_Comedy_movies}
+      />
+      <Row
+        title="Horror"
+        backdrop={true}
+        fetchurl={requist.fetcht_Horror_movies}
+      />
+
+      <Row
+        title="Romantic"
+        backdrop={true}
+        fetchurl={requist.fetcht_Romantic_movies}
+      />
+      <Row
+        title="Documentries"
+        backdrop={true}
+        fetchurl={requist.fetcht_Documentaries}
+      />
     </div>
-  )
-
-
+  );
 }
 
-
-export default App
-
-
+export default App;
 
 //tmdb api:- 8ef30a39bd41ae9b8b5105a0f18e5deb
 
--- src/Row/Row.jsx
+++ src/Row/Row.jsx
@@ -1,90 +1,107 @@
 import React, { useEffect, useState } from "react";
 import instance from "../axios";
-import './Row.css'
+import "./Row.css";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
 
+function Row({ title, fetchurl, backdrop }) {
+  const [movies, setmovies] = useState([]);
+  const [videoId, setvideoid] = useState(null);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVarse: {
+      autoplay: 1,
+    },
+  };
 
-function Row({ title, fetchurl, backdrop}) {
-    const [movies, setmovies] = useState([])
-    const [videoId,setvideoid] = useState(null)
-    const opts = {
-        height:"390",
-        width:"100%",
-        playerVarse:{
-            autoplay:1
+  useEffect(() => {
+    async function fetchingdata() {
+      const request = await instance.get(fetchurl);
+      setmovies(request.data.results);
+      return request;
+    }
+    fetchingdata();
+  }, [fetchurl]);
+
+  // create trailer
+
+  function handlesubmit(movie) {
+    if (videoId) {
+      setvideoid("");
+    } else {
+      movieTrailer(movie?.name || movie.title)
+        .then((url) => {
+          // console.log(url)
+          console.log("url:- ", url);
+          // console.log(movie)
+          let check = url.split("v=")[1].substring(0, 11);
+          setvideoid(check);
+        })
+        .catch((e) => {
+          console.log(e);
+          document.getElementById("yt_er").style.opacity = "1";
+          document.getElementById("yt_er").style.display = "block";
+        });
+    }
+  }
+
+  function clear() {
+    setvideoid("");
+    document.getElementById("yt_popup").style.display = "none";
+  }
+
+  function dlt() {
+    document.getElementById("yt_er").style.opacity = "0";
+  }
+  return (
+    <>
+      <div id="row">
+        <h2 className={`Row_title`}>{title}</h2>
+        <div id="Row">
+          {movies.map((movie) => {
+            return (
+              <img
+                id="row_poster"
+                onClick={() => handlesubmit(movie)}
+                className={`${
+                  backdrop ? "row_poster" : "withlarge row_poster"
+                }`}
+                src={`https://image.tmdb.org/t/p/original${
+                  backdrop ? movie.backdrop_path : movie.poster_path
+                }`}
+                alt={movie.name}
+                key={movie.id}
+              />
+            );
+          })}
+          {console.log(movies)}
+        </div>
+        {videoId && (
+          <div id="yt_popup" className="yt_div">
+            <YouTube id="youtube" videoId={videoId} opts={opts} />
+            <button className="yt_btn" onClick={() => clear()}>
+              {" "}
+              &#x274C;
+            </button>
+          </div>
+        )}
+        {
+          <p id="yt_er" className="trailer_er">
+            Can not find trailer !{" "}
+            <button
+              className="btn"
+              onClick={() => {
+                dlt();
+              }}
+            >
+              &#x274C;
+            </button>{" "}
+          </p>
         }
-    }
-
-    useEffect(() => {
-        async function fetchingdata() {
-            const request = await instance.get(fetchurl)
-            setmovies(request.data.results)
-            return request
-        }
-        fetchingdata()
-    }, [fetchurl])
-     
-    // create trailer
-
-    function handlesubmit(movie){
-        if(videoId){
-            setvideoid("")
-        }else{
-            movieTrailer(movie?.name||movie.title).then((url)=>{
-                // console.log(url)
-                console.log("url:- ",url)
-                // console.log(movie)
-                let check = url.split("v=")[1].substring(0,11)
-                setvideoid(check)
-            }).catch((e)=>{
-                console.log(e)
-                document.getElementById("yt_er").style.opacity="1"
-            })
-        }
-    }
-
-    function clear(){
-        setvideoid("")
-        document.getElementById("yt_popup").style.display="none"
-    }
-
-    function dlt(){
-        document.getElementById("yt_er").style.opacity="0"
-    }
-    return (
-        <>
-            <div id="row">
-                <h2 className={`Row_title`}>{title}</h2>
-                <div id="Row" >
-                    {   
-                        movies.map((movie) => {
-                            return (                            
-                            <img id="row_poster" onClick={()=>handlesubmit(movie)} className={`${backdrop? "row_poster" : "withlarge row_poster"}`} src={`https://image.tmdb.org/t/p/original${backdrop?movie.backdrop_path:movie.poster_path}`} alt={movie.name} key={movie.id}/>
-                            )
-                        })
-                    }
-                    {console.log(movies)}
-
-                </div>
-                {videoId &&
-                    <div id="yt_popup" className="yt_div">
-                        <YouTube id="youtube"
-                        videoId={videoId} opts={opts}
-                        
-                    />
-                    <button className="yt_btn" onClick={()=>clear()}> &#x274C;</button>
-                    </div>
-                    
-                    
-                }
-                {<p id="yt_er" className="trailer_er">Can not find trailer ! <button className="btn" onClick={()=>{dlt()}}>&#x274C;</button> </p>}
-                
-            </div>
-            
-            
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Row
+export default Row;
